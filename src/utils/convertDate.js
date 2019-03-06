e3a03a78eeceebdab3f7383325fd39aec4303c86
kboul <kos.voulgaridis@gmail.com>
2019-03-06 16:43:33 +0200

    Create convertDate.js to be able to covert YY-MM-DD to dates of type 'February 8, 2019'

---
+++ src/utils/convertDate.js
@@ -0,0 +1,19 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+export const convertDate = releaseDate => {
+    const temp_date = releaseDate.split('-')
+    return `${months[Number(temp_date[1]) - 1]} ${temp_date[2]}, ${temp_date[0]}`
+}
