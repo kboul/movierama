d15aa2dee7d4d8d6b570d0f04e264c31cfb14d21
kboul <kos.voulgaridis@gmail.com>
2019-03-06 11:10:46 +0200

    Create httpService to hold baseApiUrl & API_KEY

---
+++ src/services/httpService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
+
+export const API_KEY = '403de008d246c1252605ab3f69af4bb8'
+
+export default {
+    get: axios.get
+}
