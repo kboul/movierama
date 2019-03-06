3998528aaddfc5fc536ee294970b6e008919c2f2
kboul <kos.voulgaridis@gmail.com>
2019-03-06 12:54:11 +0200

    Make an http call to receive moviesNowPlaying & build the UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import "./styles.css"
+import { getMoviesNowPlaying } from './services/moviesNowPlayingService'
+import { moviesCard } from './htmlChunks/moviesCards'
 
-document.getElementById("app").innerHTML = `<h1>Hello World!</h1>`
+let moviesNowPlaying = []
+
+getMoviesNowPlaying().then(response => {
+    const { data: { results } } = response
+    moviesNowPlaying = [...results]
+    buildMovieCards(moviesNowPlaying)
+})
+
+const buildMovieCards = movies =>
+    document.getElementById("cardContainer").innerHTML = moviesCard(movies)
