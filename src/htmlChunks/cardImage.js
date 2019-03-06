95fd20d317558a9de6e646c4c9670b484a2890e0
kboul <kos.voulgaridis@gmail.com>
2019-03-06 16:26:13 +0200

    Give identical img sizes as the original portal

--- src/htmlChunks/cardImage.js
+++ src/htmlChunks/cardImage.js
@@ -1,5 +1,5 @@
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w185'
 
 export const cardImage = posterPath => {
-    return `<img width="200" height="250" src="${imageBaseUrl}/${posterPath}" />`
+    return `<img width="185" height="278" src="${imageBaseUrl}/${posterPath}" />`
 }
