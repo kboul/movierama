617f00aa215744d76f1d5f4590d67bb1c382f403
kboul <kos.voulgaridis@gmail.com>
2019-03-07 16:47:37 +0200

    Visualize the entire circle with grey color when vote_average is 0, display NR as text inside the circle without %

--- src/htmlChunks/circularBar.js
+++ src/htmlChunks/circularBar.js
@@ -1,9 +1,13 @@
-const chooseVoteColor = votePercentage => `${votePercentage > 70 ? '#09b962' : '#d2d531'}`
+const chooseVoteColor = votePercentage => {
+    return `${votePercentage > 70 ? '#09b962' :
+        votePercentage > 0 && votePercentage < 70 ? '#d2d531' :
+            'rgb(128, 128, 128)'}`
+}
 
 export const circularBar = votePercentage => {
     const htmlChunk = `
         <div class="progress-circle over50 p${votePercentage}">
-            <span>${votePercentage}%</span>
+            <span>${votePercentage != 0 ? `${votePercentage}%` : 'NR'}</span>
             <div class="left-half-clipper">
                 <div 
                     class="first50-bar"
