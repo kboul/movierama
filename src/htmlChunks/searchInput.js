e9c9abda89f31f3c250c25e602ec2f8bcb25ee9f
kboul <kos.voulgaridis@gmail.com>
2019-03-08 12:39:02 +0200

    Create searchInput htmlChunk

---
+++ src/htmlChunks/searchInput.js
@@ -0,0 +1,13 @@
+export const searchInput = () => {
+    return `
+        <div class="input-group-prepend">
+            <span class="input-group-text fa fa-search py-2"></span>
+        </div>
+        <input 
+            type="text" 
+            class="form-control" 
+            placeholder="Search for a movie..." 
+            aria-label="Search"
+            aria-describedby="addon-wrapping">
+    `
+}
