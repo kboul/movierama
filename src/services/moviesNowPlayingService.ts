1007997680b33a46e518e2489d8f49b6a321b36d
kboul <kos.voulgaridis@gmail.com>
2019-03-09 15:42:24 +0200

    Make page type number on searchMovies & moviesNowPlaying

--- src/services/moviesNowPlayingService.ts
+++ src/services/moviesNowPlayingService.ts
@@ -3,5 +3,5 @@ import http, { API_KEY } from './httpService'
 const apiEndpoint: string
     = `movie/now_playing?api_key=${API_KEY}&language=en-US`
 
-export const getMoviesNowPlaying = (page: string) =>
+export const getMoviesNowPlaying = (page: number) =>
     http.get(`${apiEndpoint}&page=${page}`)
--- src/services/searchMovies.ts
+++ src/services/searchMovies.ts
@@ -3,5 +3,5 @@ import http, { API_KEY } from './httpService'
 const apiEndpoint: string =
     `search/movie?api_key=${API_KEY}&language=en-US&include_adult=false`
 
-export const getSearchMovies = (page: string, query: string) =>
+export const getSearchMovies = (page: number, query: string) =>
     http.get(`${apiEndpoint}&page=${page}&query=${query}`)
