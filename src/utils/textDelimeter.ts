71401379c8afd6b7dca3069cde307478d09973d7
kboul <kos.voulgaridis@gmail.com>
2019-03-10 11:34:09 +0200

    Shrink the text when words length are > than 35

--- src/utils/textDelimeter.ts
+++ src/utils/textDelimeter.ts
@@ -4,12 +4,12 @@ export const textDelimeter = (text: string): string => {
 
     // if the text.length is not bigger than 39 
     // return the original text
-    if (wordSplit.length < 39) return text
+    if (wordSplit.length < 35) return text
 
-    // find the first 39 words, store them
+    // find the first 35 words, store them
     // and then present them i nthe UI with 3 dots
     wordSplit.forEach((el, i) => {
-        if (i < 39) words.push(el)
+        if (i < 35) words.push(el)
     })
     return `${words.join(' ')}...`
 }
