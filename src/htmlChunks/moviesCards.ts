50d4dcdaf1dad69b480a2e807fdb1328b7dcb074
kboul <kos.voulgaridis@gmail.com>
2019-03-10 11:33:31 +0200

    Display the first 3 genres under movie date, limit the margin between overview & genres

--- src/htmlChunks/moviesCards.ts
+++ src/htmlChunks/moviesCards.ts
@@ -3,6 +3,16 @@ import { cardImage } from './cardImage'
 import { convertDate } from "../utils/convertDate"
 import { textDelimeter } from "../utils/textDelimeter"
 import { Movies } from '../interfaces/movies'
+import { getGenres } from '../services/genres'
+import { Genres } from '../interfaces/genres'
+import { displayGenresNames } from '../utils/findGenres'
+
+let movieGenres: Array<Genres> = []
+
+getGenres().then(response => {
+    const { data: { genres } } = response
+    movieGenres = [...genres]
+})
 
 export const moviesCard = (movies: Array<Movies>): string => {
     if (movies.length === 0)
@@ -10,7 +20,7 @@ export const moviesCard = (movies: Array<Movies>): string => {
 
     let htmlChunk: string = ''
 
-    movies.forEach(({ vote_average, title, release_date, overview, poster_path }) => {
+    movies.forEach(({ vote_average, title, release_date, overview, poster_path, genre_ids }) => {
         const votePercentage = vote_average * 10
 
         htmlChunk += `
@@ -25,21 +35,17 @@ export const moviesCard = (movies: Array<Movies>): string => {
                                 ${circularBar(votePercentage)}
                             </div>
                             <div class="col-10">
-                                <div class="row">
-                                    <div class="col-8">
-                                        <a id="movieTitle">
-                                            ${title}
-                                        </a>
-                                        <span id="date">
-                                            ${release_date ? convertDate(release_date) : ''}
-                                        </span>
-                                    </div>
-                                    <div class="col-4 text-right">
-                                        right
-                                    </div>
-                                </div>
+                                <a id="movieTitle">
+                                    ${title}
+                                </a>
+                                <span id="date">
+                                    ${release_date ? convertDate(release_date) : ''}
+                                </span>
                             </div>
                         </div>
+                       <span id="genres">
+                            ${displayGenresNames(movieGenres, genre_ids)}
+                       </span>
                         <p class="card-text mb-auto" id="overview">
                             ${textDelimeter(overview)}
                         </p>
--- src/utils/findGenres.ts
+++ src/utils/findGenres.ts
@@ -1,8 +1,15 @@
 import { Genres } from '../interfaces/genres'
 
-// returns the array of specific genres that correspond to each movie
-export const findGenres = (movieGenres: Array<Genres>, genresList: Array<Number>) => {
+// returns the array of specific genres objects that correspond to each movie
+const findGenres = (movieGenres: Array<Genres>, genresList: Array<Number>): Array<Genres> => {
     return movieGenres.filter(item => {
         return (genresList.indexOf(item.id) !== -1)
     })
+}
+
+// returns the first 3 genres with spaces between them
+export const displayGenresNames = (movieGenres: Array<Genres>, genresList: Array<Number>): string => {
+    return findGenres(movieGenres, genresList)
+        .map((genre, index) => index < 3 ? ' ' + genre.name : '')
+        .join(' ')
 }
