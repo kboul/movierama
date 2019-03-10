96adf97614b422383a00fa53e38cf83dfe9cbc07
kboul <kos.voulgaridis@gmail.com>
2019-03-10 10:25:14 +0200

    Create genres service

---
+++ src/services/genres.ts
@@ -0,0 +1,6 @@
+import http, { API_KEY } from './httpService'
+
+const apiEndpoint: string =
+    `genre/movie/list?api_key=${API_KEY}&language=en-US`
+
+export const getGenres = () => http.get(apiEndpoint)
