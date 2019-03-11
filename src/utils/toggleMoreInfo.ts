a96c077ed254b34353bfed7578d61ad014adf9df
kboul <kos.voulgaridis@gmail.com>
2019-03-11 16:48:58 +0200

    Add an explanation comment & types on toggleMoreInfo

--- src/utils/toggleMoreInfo.ts
+++ src/utils/toggleMoreInfo.ts
@@ -3,10 +3,12 @@ import { removeModalFromDom } from './removeModalFromDom'
 import { modal } from '../htmlChunks/modal'
 import { buildModal } from '../renderHtml/buildModal'
 
+// makes sures DOM is not polluted with multiples modal elements
+// and renders the appropriate every time based on clicked movie id
 export const toggleMoreInfo = () => {
     const moreInfo = document.getElementsByClassName('more-info')
     const moreInfoElements = Array.from(moreInfo)
-    moreInfoElements.forEach((el, i) => {
+    moreInfoElements.forEach((el: HTMLElement, i: number) => {
         el.addEventListener('click', () => {
             removeModalFromDom()
             buildModal(modal(moreInfo[i].id))
