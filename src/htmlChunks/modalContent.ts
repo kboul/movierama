b220b7cee71786a7feb1c0241effb3b4be28e2fa
kboul <kos.voulgaridis@gmail.com>
2019-03-11 20:54:03 +0200

    Include cards with similar movies inside modal when more info click

--- src/htmlChunks/modalContent.ts
+++ src/htmlChunks/modalContent.ts
@@ -1,22 +1,26 @@
 import { card } from "./card"
 import { Reviews } from '../interfaces/reviews'
+import { Movies } from '../interfaces/movies'
+import { similarMoviesCard } from './similarMoviesCard'
 
-export const modalContent = (reviews: Array<Reviews>) => {
+export const modalContent = (reviews: Array<Reviews>, similarMovies: Array<Movies>) => {
     let modalContent: HTMLElement = document.createElement('div')
     modalContent.innerHTML = `
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                 <a class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" role="tab" aria-controls="nav-home" >Home</a>
-                <a class="nav-item nav-link active" id="nav-profile-tab" data-toggle="tab" role="tab" aria-controls="nav-profile" >Reviews</a>
-                <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" role="tab" aria-controls="nav-contact">Contact</a>
+                <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" role="tab" aria-controls="nav-profile" >Reviews</a>
+                <a class="nav-item nav-link active" id="similar-movies-tab" data-toggle="tab" role="tab" aria-controls="similar-movies">Similar Movies</a>
             </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-            <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
                 ${card(reviews)}
             </div>
-            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+            <div class="tab-pane fade show active" id="similar-movies" role="tabpanel" aria-labelledby="similar-movies-tab">
+                ${similarMoviesCard(similarMovies)}
+            </div>
         </div>
     `
 
--- src/utils/toggleMoreInfo.ts
+++ src/utils/toggleMoreInfo.ts
@@ -4,6 +4,9 @@ import { modal } from '../htmlChunks/modal'
 import { buildModal } from '../renderHtml/buildModal'
 import { getReviews } from '../services/reviewsService'
 import { modalContent } from '../htmlChunks/modalContent'
+import { getSimilarMovies } from '../services/similarMoviesService';
+import { Reviews } from '../interfaces/reviews'
+import { Movies } from '../interfaces/movies'
 
 // makes sures DOM is not polluted with multiples modal elements
 // and renders the appropriate every time based on clicked movie id
@@ -11,11 +14,19 @@ export const toggleMoreInfo = () => {
     const moreInfo = document.getElementsByClassName('more-info')
     Array.from(moreInfo).forEach((el: HTMLElement) => {
         el.addEventListener('click', () => {
+            let reviews: Array<Reviews> = []
+            let similarMovies: Array<Movies> = []
+            getSimilarMovies(el.id).then(response => {
+                similarMovies = [...response.data.results]
+            })
             getReviews(el.id).then(response => {
-                modalContent(response.data.results)
+                reviews = [...response.data.results]
             })
             removeModalFromDom()
             buildModal(modal(el.id))
+            setTimeout(()=> {
+                modalContent(reviews, similarMovies)
+            }, 200)
         })
     })
 }
