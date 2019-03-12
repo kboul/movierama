12cb43ac3452440ad65b08fcfb9d9c48603de07a
kboul <kos.voulgaridis@gmail.com>
2019-03-12 12:20:43 +0200

    Unit test convertDate function

---
+++ src/test/convertDate.test.js
@@ -0,0 +1,6 @@
+
+const { convertDate } = require('../utils/convertDate')
+
+it('converts the date from YYYY-MM-DD format to month DD, YYYY', () => {
+    expect(convertDate('2019-03-12')).toBe('March 12, 2019')
+})
