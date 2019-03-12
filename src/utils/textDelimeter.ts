789b17eb6f2d9b85019ee07157f484b60888c7ca
kboul <kos.voulgaridis@gmail.com>
2019-03-12 10:10:45 +0200

    Correct some comments on textDelimeter

--- src/utils/textDelimeter.ts
+++ src/utils/textDelimeter.ts
@@ -2,12 +2,12 @@ export const textDelimeter = (text: string): string => {
     const wordSplit = text.split(" ")
     let words: Array<string> = []
 
-    // if the text.length is not bigger than 39 
+    // if the text.length is not bigger than 35
     // return the original text
     if (wordSplit.length < 35) return text
 
     // find the first 35 words, store them
-    // and then present them i nthe UI with 3 dots
+    // and then present them in the UI with 3 dots
     wordSplit.forEach((el, i) => {
         if (i < 35) words.push(el)
     })
