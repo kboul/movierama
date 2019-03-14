a2fabc33fe383bd9b7be73e49f5092af1323603a
kboul <kos.voulgaridis@gmail.com>
2019-03-14 15:36:22 +0200

    Write unit tests for searchInput.js

---
+++ src/htmlChunks/tests/searchInput.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { searchInput } = require('../searchInput')
+
+let searchHtmlElement
+let searchInputIcon
+
+beforeAll(() => {
+    const stringHtml = searchInput()
+    const parentDiv = document.createElement('div')
+    parentDiv.innerHTML = stringHtml
+    searchHtmlElement = parentDiv.querySelector('input')
+    searchInputIcon = parentDiv.querySelector('span')
+})
+
+it('should contain an input element of type text with specific placeholder value', () => {
+    expect(searchHtmlElement.tagName).toBe('INPUT')
+    expect(searchHtmlElement.type).toBe('text')
+    expect(searchHtmlElement.placeholder).toEqual('Search for a movie...')
+})
+
+it('should have a span element with specific class selector names', () => {
+    expect(searchInputIcon.tagName).toBe('SPAN')
+    expect(searchInputIcon.className).toContain('fa fa-search py-2')
+})
