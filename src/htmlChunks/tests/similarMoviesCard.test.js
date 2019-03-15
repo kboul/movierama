b6fb93baf08eb925240bcb848952da8440fba917
kboul <kos.voulgaridis@gmail.com>
2019-03-15 11:35:21 +0200

    Export movies array & place it under testForUtils folder on similarMoviesCard.test.js

--- src/htmlChunks/tests/similarMoviesCard.test.js
+++ src/htmlChunks/tests/similarMoviesCard.test.js
@@ -3,29 +3,7 @@
  */
 
 const { similarMoviesCard } = require('../similarMoviesCard')
-
-const movies = [
-    {
-        'genre_ids': [12, 14, 878],
-        'id': 1924,
-        'original_title': "Superman",
-        'overview': "Mild-mannered Clark Kent works as a reporter at the Daily.",
-        'vote_average': 7.2,
-        'poster_path': "/n2DOECThGG7h7m5AjLi2Nuh23u1.jpg",
-        'release_date': "1978-12-13",
-        'title': "Superman"
-    },
-    {
-        'genre_ids': [18],
-        'id': 1924,
-        'original_title': "Creed II",
-        'overview': "Between personal obligations and training for his next big fight against an opponent with ties to his family's past.",
-        'vote_average': 6.6,
-        'poster_path': "/v3QyboWRoA4O9RbcsqH8tJMe8EB.jpg",
-        'release_date': "2018-12-13",
-        'title': "Creed II"
-    }
-]
+const { movies } = require('../../utilsForTests/movies')
 
 it('creates 2 cards elements with correct movie title & movie overview', () => {
     const stringHtml = similarMoviesCard(movies)
