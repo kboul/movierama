61f027a45e96007171c7545542d5748df5b0f968
kboul <kos.voulgaridis@gmail.com>
2019-03-15 15:31:55 +0200

    Create mock reviews array & place it inside utilsForTests

---
+++ src/utilsForTests/reviews.js
@@ -0,0 +1,4 @@
+exports.reviews = [
+    { 'author': 'jj@LA', content: 'I would not pay to see it. I did see it for free already...' },
+    { 'author': 'kidchaos313', content: 'Went in not paying attention to any of the drama surrounding the movie.' }
+]
