9dada7a5fbf06418028d504f0ead179022f6ad93
kboul <kos.voulgaridis@gmail.com>
2019-03-15 13:59:20 +0200

    Avoid color duplication on circularBar.test.js

--- src/htmlChunks/tests/circularBar.test.js
+++ src/htmlChunks/tests/circularBar.test.js
@@ -7,21 +7,24 @@ const { backgroundColor, borderColor, percentageSpan, }
 
 it('has green color when vote percentage is bigger than 70 & displays it with %', () => {
     const percentage = 71
-    expect(backgroundColor(percentage)).toBe('rgb(9, 185, 98)')
-    expect(borderColor(percentage)).toBe('rgb(9, 185, 98)')
+    const greenRGB = 'rgb(9, 185, 98)'
+    expect(backgroundColor(percentage)).toBe(greenRGB)
+    expect(borderColor(percentage)).toBe(greenRGB)
     expect(percentageSpan(percentage)).toEqual(`${percentage}%`)
 })
 
 it('has yellow color when percentage is smaller than 70 & displays it with %', () => {
     const percentage = 39
-    expect(backgroundColor(percentage)).toBe('rgb(210, 213, 49)')
-    expect(borderColor(percentage)).toBe('rgb(210, 213, 49)')
+    const yellowRGB = 'rgb(210, 213, 49)'
+    expect(backgroundColor(percentage)).toBe(yellowRGB)
+    expect(borderColor(percentage)).toBe(yellowRGB)
     expect(percentageSpan(percentage)).toEqual(`${percentage}%`)
 })
 
 it('has grey color when percentage is 0 & displays NR instead of percentage number with %', () => {
     const percentage = 0
-    expect(backgroundColor(percentage)).toBe('rgb(128, 128, 128)')
-    expect(borderColor(percentage)).toBe('rgb(128, 128, 128)')
+    const greyRGB = 'rgb(128, 128, 128)'
+    expect(backgroundColor(percentage)).toBe(greyRGB)
+    expect(borderColor(percentage)).toBe(greyRGB)
     expect(percentageSpan(percentage)).toEqual('NR')
 })
