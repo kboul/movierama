fd5f0d92b2701ab2ba366a603d94ada698d99567
kboul <kos.voulgaridis@gmail.com>
2019-03-15 15:59:05 +0200

    Add a third review on reviews.js, add '' on content

--- src/utilsForTests/reviews.js
+++ src/utilsForTests/reviews.js
@@ -1,4 +1,5 @@
 exports.reviews = [
-    { 'author': 'jj@LA', content: 'I would not pay to see it. I did see it for free already...' },
-    { 'author': 'kidchaos313', content: 'Went in not paying attention to any of the drama surrounding the movie.' }
+    { 'author': 'jj@LA', 'content': 'I would not pay to see it. I did see it for free already...' },
+    { 'author': 'kidchaos313', 'content': 'Went in not paying attention to any of the drama surrounding the movie.' },
+    { 'author': 'msbreviews', 'content': 'If you enjoy reading my Spoiler-Free reviews, please follow my blog' }
 ]
