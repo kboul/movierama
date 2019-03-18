f1f1d840776f744dd8f6b5160633b0c9413f5a68
kboul <kos.voulgaridis@gmail.com>
2019-03-18 14:03:36 +0200

    Unit test for buildModal.ts

---
+++ src/renderHtml/tests/buildModal.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { buildModal } = require('../buildModal')
+const { modal } = require('../../htmlChunks/modal')
+const { videos } = require('../../utilsForTests/videos')
+const { reviews } = require('../../utilsForTests/reviews')
+const { movies } = require('../../utilsForTests/movies')
+
+const fs = require('fs')
+const path = require('path')
+const html = fs.readFileSync(path.resolve(__dirname, '../../../index.html'), 'utf8')
+
+jest.dontMock('fs')
+
+beforeEach(() => {
+    document.documentElement.innerHTML = html.toString()
+})
+
+afterEach(() => {
+    jest.resetModules()
+})
+
+it('', () => {
+    const fakeMovieId = '299537'
+    buildModal(modal(fakeMovieId, videos, reviews, movies))
+    expect(document.getElementById(`modal_dialog_${fakeMovieId}`)).toBeTruthy()
+})
