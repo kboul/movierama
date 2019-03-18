31b423d30f38d10d4a21f2e4fa8ae6e3fc4b2ddc
kboul <kos.voulgaridis@gmail.com>
2019-03-18 14:04:59 +0200

    Unit test description for buildModal.ts

--- src/renderHtml/tests/buildModal.test.js
+++ src/renderHtml/tests/buildModal.test.js
@@ -22,7 +22,7 @@ afterEach(() => {
     jest.resetModules()
 })
 
-it('', () => {
+it('creates a modal by appending it under div with class name container', () => {
     const fakeMovieId = '299537'
     buildModal(modal(fakeMovieId, videos, reviews, movies))
     expect(document.getElementById(`modal_dialog_${fakeMovieId}`)).toBeTruthy()
