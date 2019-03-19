a32cb2963c3f3860909f1c2ad4d67dd5b7d83bcc
kboul <kos.voulgaridis@gmail.com>
2019-03-19 16:07:00 +0200

    Refactor findGenres.test.js, place genreMovieList & movieGenreIds on separate files under utilsForTests folder

--- src/utils/tests/findGenres.test.js
+++ src/utils/tests/findGenres.test.js
@@ -1,14 +1,9 @@
 const { displayGenresNames } = require('../findGenres')
-
-const genreMovieList = [
-    { 'id': 28, 'name': 'Action' },
-    { 'id': 12, 'name': "Adventure" },
-    { 'id': 878, 'name': "Science Fiction" },
-    { 'id': 36, 'name': "History" }
-]
-
-const movieGenreIds = [28, 12, 878, 36]
-
-it('should return up to 3 specific movie genres based on id comparison between generic list of genres & specific', () => {
-    expect(displayGenresNames(genreMovieList, movieGenreIds)).toEqual(' Action  Adventure  Science Fiction ')
-})
+const { genreMovieList } = require('../../utilsForTests/genreMovieList')
+const { movieGenreIds } = require('../../utilsForTests/movieGenreIds')
+it(`should return up to 3 specific movie genres based on id comparison 
+    between generic list of genres & specific`, () => {
+        expect(displayGenresNames(genreMovieList, movieGenreIds))
+            .toEqual(' Action  Adventure  Science Fiction ')
+    }
+)
---
+++ src/utilsForTests/genreMovieList.js
@@ -0,0 +1,6 @@
+exports.genreMovieList = [
+    { 'id': 28, 'name': 'Action' },
+    { 'id': 12, 'name': "Adventure" },
+    { 'id': 878, 'name': "Science Fiction" },
+    { 'id': 36, 'name': "History" }
+]
